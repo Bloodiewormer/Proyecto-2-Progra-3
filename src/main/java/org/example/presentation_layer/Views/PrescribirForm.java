940a2a4dc6377c51d0c10cbd2e9b4751ac6e90ca
David González Córdoba <106786019+Bloodiewormer@users.noreply.github.com>
2025-09-11 21:16:47 -0600

    Refactor PrescribirForm layout and components for improved functionality and user experience

--- src/main/java/org/example/presentation_layer/Views/PrescribirForm.java
+++ src/main/java/org/example/presentation_layer/Views/PrescribirForm.java
@@ -1,5 +1,8 @@
 package org.example.presentation_layer.Views;
 
+import com.toedter.calendar.JDateChooser;
+import org.example.presentation_layer.Components.BlueRoundedButton;
+import org.example.presentation_layer.Components.CustomTextField;
 import org.example.presentation_layer.Models.MedicamentoTableModel;
 import org.example.presentation_layer.Models.PacienteTableModel;
 import org.example.service_layer.MedicamentoService;
@@ -7,12 +10,14 @@ import org.example.service_layer.PacienteService;
 import org.example.service_layer.UsuarioService;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
 
 public class PrescribirForm {
     private JPanel mainPanel;
     private JPanel controlPanel;
-    private JButton button1;
-    private JButton button2;
+    private JButton buscarPacienteButton;
+    private JButton agregarMedicamentoButton;
     private JPanel RectaPanel;
     private JTable MedicamentoRecetatable;
     private JButton guardarButton;
@@ -33,15 +38,61 @@ public class PrescribirForm {
     private JButton guardarButton1;
     private JButton cancerlarButton;
     private JPanel DetallePanel;
+    private JButton cancelButton;
+    private JButton guardarPacienteButton;
+    private JButton cancelMedicamentoButton;
+    private JButton guardarMedicamentoButton;
+    private JPanel window;
+    private JPanel DatePickerPanel;
+
+    private JDateChooser DatePicker;
 
     public PrescribirForm(UsuarioService usuarioService, PacienteService pacienteService, MedicamentoService medicamentoService, int idMedico) {
 
+
+        initDatePickers();
         MedicamentosTable.setModel(new MedicamentoTableModel(medicamentoService.leerTodos()));
         pacientesTable.setModel(new PacienteTableModel(pacienteService.leerTodos()));
 
     }
 
+    private void initDatePickers() {
+        DatePickerPanel.setSize(300, 30);
+        DatePickerPanel.setLayout(new BorderLayout());
+
+        DatePicker = new JDateChooser();
+        DatePicker.setSize(300, 30);
+        DatePicker.setDate(new Date());
+        DatePickerPanel.add(DatePicker, BorderLayout.CENTER);
+
+        DatePickerPanel.revalidate();
+        DatePickerPanel.repaint();
+    }
+
     public  JPanel getMainPanel() {
         return mainPanel;
+    }
+
+    private void createUIComponents() {
+        //all buttons are blue rounded buttons
+        buscarPacienteButton = new BlueRoundedButton( "Buscar Paciente") ;
+        agregarMedicamentoButton = new BlueRoundedButton( "Agregar Medicamento") ;
+        guardarButton = new BlueRoundedButton( "Guardar") ;
+        detallesButton = new BlueRoundedButton( "Detalles") ;
+        limpiarButton = new BlueRoundedButton( "Limpiar") ;
+        descartarButton = new BlueRoundedButton( "Descartar") ;
+        guardarButton1 = new BlueRoundedButton( "Guardar") ;
+        cancerlarButton = new BlueRoundedButton( "Cancelar") ;
+        cancelButton = new BlueRoundedButton( "Cancelar") ;
+        guardarPacienteButton = new BlueRoundedButton( "Guardar") ;
+        cancelMedicamentoButton = new BlueRoundedButton( "Cancelar") ;
+        guardarMedicamentoButton = new BlueRoundedButton( "Guardar") ;
+        //all text fields are rounded
+        busquedaPacientetextField = new CustomTextField() ;
+        BusquedaMedicamentoTextField = new CustomTextField() ;
+
+
+
+
     }
 }
