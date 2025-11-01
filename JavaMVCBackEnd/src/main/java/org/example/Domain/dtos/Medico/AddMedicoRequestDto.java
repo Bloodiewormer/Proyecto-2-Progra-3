8423eb3b40c7e3c2535cb30561c21860643a6bb3
EmmanuelNJ02 <emmanueljnj02@gmail.com>
2025-11-01 01:08:12 -0600

    Dto Medico y ListMedicamentoResponseDto. Revisar.

---
+++ JavaMVCBackEnd/src/main/java/org/example/Domain/dtos/Medico/AddMedicoRequestDto.java
@@ -0,0 +1,29 @@
+package org.example.Domain.dtos.Medico;
+
+public class AddMedicoRequestDto {
+    private int id;
+    private String nombre;
+    private String clave;
+    private String especialidad;
+
+    public AddMedicoRequestDto() {}
+
+    public AddMedicoRequestDto(int id, String nombre, String clave, String especialidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.clave = clave;
+        this.especialidad = especialidad;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getClave() { return clave; }
+    public void setClave(String clave) { this.clave = clave; }
+
+    public String getEspecialidad() { return especialidad; }
+    public void setEspecialidad(String especialidad) { this.especialidad = especialidad; }
+}
